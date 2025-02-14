a0904e63eed02e5f15795f0f281e044b46289c17
Oscar Araya <oscarin821@gmail.com>
2025-02-14 17:52:19 -0600

    Cambio del request a curl

--- Tarea1/mixer.py
+++ Tarea1/mixer.py
@@ -1,10 +1,11 @@
 import os
 import requests
+import subprocess
 from cryptography.hazmat.primitives.asymmetric import rsa, padding as async_padding
 from cryptography.hazmat.primitives import serialization, hashes, padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
-# Cargar una clave pública desde un archivo PEM
+# Cargar la llave publica desde el archivo PEM
 # https://cryptography.io/en/latest/hazmat/primitives/asymmetric/serialization/
 def load_public_key(pem_path):
     with open(pem_path, "rb") as f:
@@ -41,41 +42,63 @@ def cifrado_rsa(public_key, data):
 # Función para enviar mensaje al mixnet
 def enviar_mensaje(url, port, encrypted_message):
     try:
-        response = requests.post(f"{url}:{port}", data=encrypted_message, verify=False)
-        print(f"Respuesta del mixnet ({url}:{port}):", response.text)
-    except requests.exceptions.RequestException as e:
-        print(f"Error al enviar al mixnet ({url}:{port}): {e}")
+        curl_command = [
+            "curl", "-k",
+            "-X", "POST",
+            f"{url}:{port}",
+            "-H", "Content-Type: application/octet-stream",
+            "--data-binary", "@-"
+        ]
 
-# Cargar claves públicas desde archivos PEM
+        result = subprocess.run(
+            curl_command,
+            input=encrypted_message,
+            capture_output=True,
+            text=False
+        )
+
+        response_bytes = result.stdout.strip()
+
+        if response_bytes == b"\x06":
+            print(f"Bien")
+        elif response_bytes == b"\x15":
+            print(f"Error")
+        else:
+            print(f"Default: {response_bytes.hex()}")
+
+    except Exception as e:
+        print(f"Error al enviar ({url}:{port}): {e}")
+
+# Cargar llaves publicas
 mix1 = load_public_key("public-key-mix-1.pem")
 mix2 = load_public_key("public-key-mix-2.pem")
 mix3 = load_public_key("public-key-mix-3.pem")
 
 # Mensaje
-recipient = "Alice"
+recipient = "estudiante6"
 message = "Hola"
 message = f"{recipient},{message}".encode()
 
-# Primera capa de cifrado (Mix 3)
+# Primer cifrado (Mix 3)
 key3 = os.urandom(16) # Llave Random
 iv3, cifrado_1 = cifrado_aes(message, key3)
 E1 = cifrado_rsa(mix3, iv3 + key3) + cifrado_1
 
-# Segunda capa de cifrado (Mix 2)
+# Segunda cifrado (Mix 2)
 key2 = os.urandom(16)
 iv2, cifrado_2 = cifrado_aes(E1, key2)
 E2 = cifrado_rsa(mix2, iv2 + key2) + cifrado_2
 
-# Tercera capa de cifrado (Mix 1)
+# Tercer cifrado (Mix 1)
 key1 = os.urandom(16)
 iv1, cifrado_3 = cifrado_aes(E2, key1)
 E3 = cifrado_rsa(mix1, iv1 + key1) + cifrado_3
 
-# Agregar la longitud del mensaje (4 bytes, big-endian)
+# Agrega longitud del mensaje
 message_length = len(E3).to_bytes(4, "big")
 network_message = message_length + E3
 
 # Enviar al primer MIX
 URL = ""
-PORT = 50074
-enviar_mensaje(URL, PORT, E3)
+PORT = 
+enviar_mensaje(URL, PORT, network_message)
